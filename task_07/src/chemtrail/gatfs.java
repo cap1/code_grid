022d35a3d767031bdd13e371bc679c5e03531f92
Christian Müller <cap.mueller@googlemail.com>
2012-06-19 20:38:22 +0200

    notes

--- task_07/src/chemtrail/gatfs.java
+++ task_07/src/chemtrail/gatfs.java
@@ -6,13 +6,15 @@ import org.gridlab.gat.URI;
 
 
 
-public interface gatfs {
+public interface Gatfs {
 	void createFile(URI FileName) throws Exception; //check
 	void createDir(URI DirName) throws Exception; //check 
+	//Lieber stream als parameter
 	Object readFile(URI FileName) throws Exception; 
 	ArrayList<URI> readDir(URI DirName) throws Exception; //check
 	void deleteFile(URI FileName) throws Exception;
 	void deleteDir(URI DirName) throws Exception;
+	//streams
 	void updateFile(URI Filename, Object Data) throws Exception;
 	void renameDir(URI OldDirName, URI NewDirName) throws Exception;
 	void renameFile(URI OldFileName, URI NewFileName) throws Exception;
