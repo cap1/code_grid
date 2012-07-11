543c3471d619a7ff88b7a83f3f5eed3f456228d9
Ralph Krimmel <mail@postefix.de>
2012-07-11 16:47:43 +0200

    getting closer

--- task_10/multi_pass_blocking.c
+++ task_10/multi_pass_blocking.c
@@ -21,52 +21,46 @@ int main(int argc, char *argv[])
   	
 	// Solve the assignment here
 
-	//printf("====NEXT ROUND: %d started====\n",i);
 	
-	if(myrank == 0)
-	{
-		MPI_Send(&myrank,1,MPI_INT,myrank+1,myrank,MPI_COMM_WORLD);
-		//printf("Process %d sent message to process %d\n",myrank,myrank+1);
-		//MPI_Recv(&ranksum,1,MPI_INT,recvfrom,recvfrom,MPI_COMM_WORLD,&status);
-		//Sleep to keep space-time continuum intact ;)
-		sleep(1);
-		printf("Process %d received message from process %d\n",myrank,nprocs-1);
-		printf("---Process sum is %d\n",ranksum);
-	}
 	
-	if(myrank == nprocs-1)
+	while(recvrank != myrank || round != nprocs-1)
 	{
-		sendto = 0;
-	}
-	else
-	{
-		sendto = myrank + 1;
-	}
-	
-	if(myrank == 0)
-	{
-		recvfrom = nprocs-1;
-	}
-	else
-	{
-		recvfrom = myrank-1;
-	}
-	
-	MPI_Recv(&recvrank,1,MPI_INT,recvfrom,recvfrom,MPI_COMM_WORLD,&status);
-	MPI_Send(&recvrank,1,MPI_INT,sendto,myrank,MPI_COMM_WORLD);
-	round++;
-	while(recvrank != myrank)
-	{
-		printf("ROUND NR. #%d\n",round);
+		if(myrank == nprocs-1)
+		{
+			sendto = 0;
+		}
+		else
+		{
+			sendto = myrank + 1;
+		}
+		
+		if(myrank == 0)
+		{
+			recvfrom = nprocs-1;
+		}
+		else
+		{
+			recvfrom = myrank-1;
+		}
+
+		if(myrank == round)
+		{
+			printf("STARTING ROUND NR. #%d\n",round);
+			MPI_Send(&myrank,1,MPI_INT,sendto,myrank,MPI_COMM_WORLD);
+			//printf("Process %d sent message to process %d\n",myrank,myrank+1);
+			MPI_Recv(&recvrank,1,MPI_INT,recvfrom,recvfrom,MPI_COMM_WORLD,&status);
+			printf("Process %d received rank %d from process %d\n",myrank,recvrank,recvfrom);
+			printf("ROUND NR. #%d FINISHED\n",round);
+			continue;
+		}
 		round++;	
 		MPI_Recv(&recvrank,1,MPI_INT,recvfrom,recvfrom,MPI_COMM_WORLD,&status);
 		printf("Process %d received rank %d from process %d\n",myrank,recvrank,recvfrom);
 		MPI_Send(&recvrank,1,MPI_INT,sendto,myrank,MPI_COMM_WORLD);
 		//printf("Process %d sent message to process %d\n",myrank,sendto);
 		ranksum += recvrank;
-	
+		//printf("---Process sum is %d\n",ranksum);
 	}
-	printf("---Process sum is %d\n",ranksum);
 
 
 	MPI_Finalize();
